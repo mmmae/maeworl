75b8d0b3dc272f3dbbb7c8488892cf748cdfa94f
mmmae <119120714+mmmae@users.noreply.github.com119120714+mmmae@users.noreply.github.com>
2023-09-23 13:33:39 +1000

    Fix typos, add archive licensing, 404, new news

--- scripts/bulk-gen-imgs.py
+++ scripts/bulk-gen-imgs.py
@@ -9,4 +9,4 @@ files = [f for f in os.listdir('.') if os.path.isfile(f)]
 files.reverse()
 
 for f in files:
-    print(f'<a href="/img/archive/{f}"><img src="/img/archive/thumbs/{f}" alt="thumbnail" loading="lazy"></a>')
+    print(f'<div class="item"><a href="/img/archive/{f}"><img src="/img/archive/thumbs/{f}" alt="thumbnail" loading="lazy"></a><div class="lice" title="CC0"></div></div>')
